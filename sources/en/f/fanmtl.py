8a30654862f89ccd7336018de24c91040891bd11
1k-7 <aldrin.krypton@gmail.com>
2025-12-06 20:55:31 +0530

    update fanmtl.py

--- sources/en/f/fanmtl.py
+++ sources/en/f/fanmtl.py
@@ -32,11 +32,11 @@ class FanMTLCrawler(Crawler):
         })
         
         # Force traffic through WARP (socks5h = Remote DNS resolution)
-        self.proxy_url = "socks5h://127.0.0.1:40000"
-        self.runner.proxies = {
-            "http": self.proxy_url,
-            "https": self.proxy_url
-        }
+    #    self.proxy_url = "socks5h://127.0.0.1:40000"
+ #       self.runner.proxies = {
+#            "http": self.proxy_url,
+       #     "https": self.proxy_url
+    #    }
 
         # Optimize connection pool
         adapter = requests.adapters.HTTPAdapter(pool_connections=60, pool_maxsize=60)
@@ -58,7 +58,7 @@ class FanMTLCrawler(Crawler):
             options = ChromeOptions()
             options.add_argument("--no-sandbox") 
             options.add_argument("--disable-dev-shm-usage")
-            options.add_argument(f'--proxy-server={self.proxy_url}')
+        #    options.add_argument(f'--proxy-server={self.proxy_url}')
             
             driver = create_local(headless=True, options=options)
             
